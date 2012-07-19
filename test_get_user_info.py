9937f518c3fe3b1f9e1dedb22267349474d9f206
Max Rasskazov <mrasskazov@mirantis.com>
2012-07-19 19:31:24 +0400

    Unittest based test module - initial commit

---
+++ test_get_user_info.py
@@ -0,0 +1,29 @@
+'''
+Created on 19.07.2012
+
+@author: mrasskazov
+'''
+import unittest
+
+from config import *
+from get_user_info import *
+
+
+class TestGetUserInfo(unittest.TestCase):
+
+
+    def setUp(self):
+        pass
+
+
+    def tearDown(self):
+        pass
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
