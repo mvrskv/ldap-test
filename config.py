54fc901581f1f048c05fea0da0b6b1d165bc3798
Max Rasskazov <mrasskazov@mirantis.com>
2012-07-25 17:39:12 +0400

    Comments in config.py are brought into compliance

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ service_url = 'http://172.18.67.57:5000/v2.0/'
 
 
 #===============================================================================
-# tenants_ID = 
+# tenant_ids = 
 # {
 #    'tenant_1_name': 'tenant_1_id', 
 #    'tenant_2_name': 'tenant_2_id', 
@@ -27,14 +27,28 @@ tenant_ids = {
 #    'UserName': 
 #    {
 #        'password':'Password_phrase',
-#        'roles':
+#        'expected_result':
 #        {
-#            'g':'Global_role_name',
-#            't1':'Tenant_1_role',
-#            't2':None
+#            '<TestName>': <Expected_result>,
+#            '<TestName>': <Expected_result>,
 #        }
 #    },
 # }
+# <TestName> must coincide with 'testname' variable value, that defined in 
+# each test_ function, for example:
+#    testname = 'test_auth_w_spec_tenant'
+# <Expected_result> may be defined for each test individually, test function
+# use this. Examples:
+# 'expected_result':
+# {
+#    'test_auth_w_spec_tenant':                  {'t1':True, 't2':False},
+#    'test_auth_wo_spec_tenant':                 True,
+#    'test_get_available_tenants':               set(['t1']),
+#    'test_get_roles_in_tenant':                 {'t1':set(['admin']), 't2':False},
+# }
+# In test 'test_get_roles_in_tenant' set([]) != False
+#    'test_get_roles_in_tenant':                 {'t1':False, 't2':set([])},
+
 #===============================================================================
 
 users={
@@ -122,7 +136,7 @@ users={
                 'test_get_roles_in_tenant':                 {'t1':False, 't2':False},
             }
         },
-        # user in tenant 1 without specified role
+        # user in tenant 1 without specified roles
        't1wor': 
        {
             'password':'Mirantis123',
@@ -134,7 +148,7 @@ users={
                 'test_get_roles_in_tenant':                 {'t1':set([]), 't2':False},
             }
         },
-        # user in tenant 1 without specified role
+        # user in tenant 1 without specified roles
        't2wor': 
        {
             'password':'Mirantis123',
